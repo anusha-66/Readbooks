9a0a3e241df49c0437c4d1f29ba143ce5c892b3a
Anusha Ganti <27336160+anusha-66@users.noreply.github.com>
2020-10-16 21:18:04 -0400

    Updates

---
+++ precache-manifest.81f8119cc625c558f578a9c9aaff7466.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1de046f138d7a724e4128e8d6d02c241",
+    "url": "/Readbooks/index.html"
+  },
+  {
+    "revision": "6f9dc18e0ba10a66607d",
+    "url": "/Readbooks/static/css/main.0afb1f1f.chunk.css"
+  },
+  {
+    "revision": "7c13f56756629766cbf1",
+    "url": "/Readbooks/static/js/2.a13ce5e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Readbooks/static/js/2.a13ce5e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f9dc18e0ba10a66607d",
+    "url": "/Readbooks/static/js/main.2d26a6fa.chunk.js"
+  },
+  {
+    "revision": "4844a120d8ab360951e9",
+    "url": "/Readbooks/static/js/runtime-main.44fe6554.js"
+  }
+]);
